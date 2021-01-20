3a6924a0bcbbb68971cc1e1dbcc9ed51dad3b399
Nate <7nater.nelson@gmail.com>
2021-01-20 09:17:49 -0700

    commit tree 7c1aa2450454e98b825934981ff77577f7f115d7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    string_array = self.split("")
+    string_array.last == "."
   end
 
   def question?
-
+    string_array = self.split("")
+    string_array.last == "?"
   end
 
   def exclamation?
-
+    string_array = self.split("")
+    string_array.last == "!"
   end
 
   def count_sentences
-
+    # binding.pry
+    string_array = self.split(".")
+    string_array = string_array.join("?").split("?")
+    string_array = string_array.join("!").split("!")
+    #binding.pry
+    string_array.delete("") #If there are repeating punctuacting marks, deletes those empty arrays. Example "My mom rocks!!".split("!")  == ["My mom rocks",""].delete("") == ["My mom rocks"]
+    string_array.count
   end
 end
